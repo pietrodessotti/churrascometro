fcbbf53a944b72e90f5b3f7908f8100880f44d77
Pietro Dessotti <pietrohdessotti@gmail.com>
2021-06-03 14:00:49 -0300

    churrascometro

---
+++ scripts.js
@@ -0,0 +1,30 @@
+let inputAdultos = document.getElementById("adultos")
+let inputCriancas = document.getElementById("criancas")
+let inputDuracao = document.getElementById("duracao")
+
+let resultado = document.getElementById("resultado")
+
+function calculaResultado() {
+    let carne = 0.4;
+    let cerveja = 1.2 / 0.355;
+    let refrigerante = 1 / 2;
+
+    if (inputDuracao.value >= 6) {
+        carne = 0.65
+        cerveja = 2 / 0.335;
+        refrigerante = 1.5 / 2;
+    }
+
+    let adultos = inputAdultos.value * 1;
+    let criancas = inputCriancas.value * 0.5;
+
+    let calcCarne = (adultos + criancas) * carne;
+    let calcCerveja = adultos * cerveja;
+    let calcRefrigerante = (adultos + criancas) * refrigerante;
+
+    resultado.innerHTML = `
+    <p>${calcCarne.toFixed(2)} Kg de Carne</p>
+    <p>${Math.ceil(calcCerveja.toFixed(2))} Latas de Cerveja</p>
+    <p>${Math.ceil(calcRefrigerante.toFixed(2))} Garrafas de Refrigerante</p>
+   `
+}
